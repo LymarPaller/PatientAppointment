e279b67d688a3c0c2d1203c9ac58be9e0f69c0ce
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-10-28 18:10:07 +0800

    fix: id tag link in navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar () {
                 <a href="index.html #home" className="logo"> <i className="fas fa-heartbeat"></i> The Doctors Inc. </a>
 
                 <nav className="navbar">
-                    <a href="index.html #home">home</a>
-                    <a href="index.html #services">services</a>
-                    <a href="index.html #about">about us</a>
-                    <a href="index.html #doctors">doctors</a>
-                    <a href="index.html #book">book</a>
-                    <a href="index.html #review">review</a>
-                    <a href="index.html #news">news</a>
+                    <a href="#home">home</a>
+                    <a href="#services">services</a>
+                    <a href="#about">about us</a>
+                    <a href="#doctors">doctors</a>
+                    <a href="#book">book</a>
+                    <a href="#review">review</a>
+                    <a href="#news">news</a>
                 </nav>
 
                 <div id="menu-btn" className="fas fa-bars"></div>
